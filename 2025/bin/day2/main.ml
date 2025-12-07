82fc75d722bb7591d3399c507c81bb7831707c5e
Liam <ltwirth@asu.edu>
2025-12-06 21:24:52 -0700

    yeahp

--- 2025/bin/day2/main.ml
+++ 2025/bin/day2/main.ml
@@ -57,3 +57,73 @@ let sum_invalid_ids : int64 =
 
 let () =
   Printf.printf "Sum of invalid ids: %Ld\n" sum_invalid_ids
+
+
+(* Now to try and do part 2*)
+(* An ID is now only considered "invalid" iff the sequence gets repeated AT LEAST twice, so now my logic no longer works, as I cant just check front against back half*)
+
+
+(* cons head tail lists we meet again *)
+let compute_m (n: int) : int list =
+  let rec aux acc m =
+    if m > n/2 then List.rev acc
+        else let acc =
+               if n mod m = 0 then m :: acc
+                else acc in
+          aux acc (m + 1)
+        in 
+
+        aux [] 1
+    
+
+  
+  
+  
+let is_invalid_id_p2 (id: string) : bool =
+    let len = String.length id in
+    let possible_ms = compute_m (len) in
+        let rec check_ms ms =
+        match ms with
+        | [] -> false
+        | m :: rest ->
+            let seg = String.sub id 0 m in
+            (* Now for each m I need to walk through the string in steps of size n and compare if they are equal *)
+            let rec check_repeats pos =
+            if pos + m > len then true
+                else
+                let next = String.sub id pos m in
+                if next = seg then
+                    check_repeats(pos + m)
+                else
+                    false
+            in
+            if check_repeats m then true
+            else check_ms rest
+        in
+        check_ms possible_ms
+
+
+
+     
+let invalid_ids_p2 : int list =
+    ranges
+    |> List.concat_map (fun (start_i, end_i) ->
+             let rec loop acc n =
+             if n > end_i then List.rev acc
+             else
+                 let s = string_of_int n in
+                 if is_invalid_id_p2 s then
+                 loop (n :: acc) (n + 1)
+                 else
+                 loop acc (n + 1)
+             in
+             loop [] start_i
+         )
+let sum_invalid_ids_p2 : int64 =
+    List.fold_left
+        (fun acc id -> Int64.add acc (Int64.of_int id))
+        Int64.zero
+        invalid_ids_p2
+
+let () =
+  Printf.printf "Sum of invalid ids: %Ld\n" sum_invalid_ids_p2
